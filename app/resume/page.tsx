5b5aac894babfcf292800582224f5dfdc97aa559
Rebtem <okunolarebeccatemitope@gmail.com>
2025-02-11 16:58:10 +0100

    update

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -9,8 +9,8 @@ const Resume: FC = () => {
         <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">Resume</h2>
         <div className="border-b-2 w-16 mx-auto my-4"></div>
         <p className="text-lg">Motivated Junior Software Developer with a Bachelors degree in Computer Engineering.
-        Passionate Frontend Developer with experience in building dynamic and responsive web applications using Next.js, React, TypeScript, and Tailwind CSS. Skilled in Supabase for database management and authentication. Adept at creating seamless user experiences and optimizing performance. 
-        Seeking opportunities to contribute innovative solutions to development teams.</p>
+          Passionate Frontend Developer with experience in building dynamic and responsive web applications using Next.js, React, TypeScript, and Tailwind CSS. Skilled in Supabase for database management and authentication. Adept at creating seamless user experiences and optimizing performance.
+          Seeking opportunities to contribute innovative solutions to development teams.</p>
         <div className="grid grid-cols-2 gap-6 mt-6">
           <div>
             <h3 className="text-2xl font-semibold mb-4">Education</h3>
@@ -27,14 +27,30 @@ const Resume: FC = () => {
             </div>
 
             <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
-            <h4 className="text-xl font-bold"> Skills</h4>
+              <h4 className="text-xl font-bold"> Skills</h4>
               <p className="text-sm">Frontend Development</p>
               <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Next.js, React.js, TypeScript </p>
               <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Tailwind CSS, CSS3, SCSS</p>
               <p className="text-sm text-gray-500 dark:text-gray-400">✔️ JavaScript (ES6+), HTML5</p>
               <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Responsive Web Design</p>
-              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ API Integration</p>                       
-                    </div>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ API Integration</p>
+            </div>
+
+
+            <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
+              
+              <p className="text-sm">Backend & Database</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Supabase (Authentication, Database, Storage </p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ PostgreSQL (via Supabase)</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Firebase (basic familiarity)</p>
+              <p className="text-sm">Tools & Workflow</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Git & GitHub</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Vercel, Netlify</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️ Figma (for UI design interpretation)</p>
+              <p className="text-sm text-gray-500 dark:text-gray-400">✔️  Agile & Scrum</p>
+
+
+            </div>
           </div>
 
           <div>
@@ -45,7 +61,7 @@ const Resume: FC = () => {
               <p className="text-sm text-gray-500 dark:text-gray-400">2023 - Present</p>
             </div>
 
-            
+
             <h3 className="text-2xl font-semibold mb-4">Projects</h3>
             <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
               <h4 className="text-xl font-bold">Job Board</h4>
@@ -57,9 +73,9 @@ const Resume: FC = () => {
               <h4 className="text-xl font-bold">Portfolio Website</h4>
               <p className="text-sm">(Next.js, TypeScript, Supabase</p>
               <p className="text-sm text-gray-500 dark:text-gray-400">Design and deploy a personal portfolio with dark mode and smooth animations.
-                </p>
+              </p>
             </div>
-            
+
 
 
 
